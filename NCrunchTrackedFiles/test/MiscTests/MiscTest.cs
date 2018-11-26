882c075202ff70a44866c6d13b492a7faf6551d9
GreenMooseSE <31244330+greenmooseSE@users.noreply.github.com>
2018-11-26 14:04:38 +0100

    Add 2 tests [try-ncrunchTrackFiles]
    - Which currently not are treated impacted if changing the json files.

---
+++ NCrunchTrackedFiles/test/MiscTests/MiscTest.cs
@@ -0,0 +1,49 @@
+﻿namespace MiscTests
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using NUnit.Framework;
+    using StdLibWithTrackedFiles;
+
+    [TestFixture]
+    internal abstract class MiscTest
+    {
+        protected void LogIt(string msg, params object[] args)
+        {
+            if (args.Length == 0)
+                Console.WriteLine(msg);
+            else
+                Console.WriteLine(msg, args);
+        }
+
+        protected string ReadFileFromStdLibBinAssemblyResource(string relativePath)
+        {
+            string rootNamespace = typeof(Client).Namespace;
+            string resourcePath = $"{rootNamespace}." + relativePath;
+            //string resourcePath = $"{rootNamespace}." + relativePath;
+            LogIt("Reading from '{0}'.", resourcePath);
+            using (var reader =
+                new StreamReader(typeof(Client).Assembly.GetManifestResourceStream(resourcePath)))
+            {
+                string result = reader.ReadToEnd();
+                return result;
+            }
+        }
+
+        protected string ReadFileFromStdLibBinOutput(string relativePath)
+        {
+            //var slnFilePath = NCrunch.Framework.NCrunchEnvironment.GetOriginalSolutionPath();
+            string assemblyFilePath = typeof(Client).Assembly.Location;
+            string assemblyDirPath = Path.GetDirectoryName(assemblyFilePath);
+
+            string fullFilePath = Path.Combine(assemblyDirPath, relativePath);
+            LogIt("Reading from '{0}'.", fullFilePath);
+            FileAssert.Exists(fullFilePath);
+            return File.ReadAllText(fullFilePath);
+        }
+
+        protected string JsonFile2AsEmbeddedResourceName { get; } = "JsonFile2AsEmbeddedResource.json";
+        protected string JsonFileCopiedToOutputName { get; } = "JsonFile1CopiedToOutput.json";
+    }
+}
---
+++ NCrunchTrackedFiles/test/MiscTests/ReadJsonFile1Test.cs
@@ -0,0 +1,27 @@
+﻿namespace MiscTests
+{
+    using System;
+    using System.Linq;
+    using NUnit.Framework;
+
+    [TestFixture]
+    internal class ReadJsonFile1Test : MiscTest
+    {
+        [Test]
+        public void CanReadJson1FileFromFile()
+        {
+            string fileContent = ReadFileFromStdLibBinOutput("Generated\\" + JsonFileCopiedToOutputName);
+            Assert.IsNotEmpty(fileContent);
+            LogIt($"Got content:\n{fileContent}\n");
+        }
+
+        [Test]
+        public void CanReadJson1FileFromResource()
+        {
+            string fileContent =
+                ReadFileFromStdLibBinAssemblyResource("Generated." + JsonFile2AsEmbeddedResourceName);
+            Assert.IsNotEmpty(fileContent);
+            LogIt($"Got content:\n{fileContent}\n");
+        }
+    }
+}
